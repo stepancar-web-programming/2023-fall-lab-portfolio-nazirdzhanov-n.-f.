4ff63013d3236ec8ac6ab0ce31df6fa68cf4e265
Nekruz Nazirjonov <92411934+nekruz03@users.noreply.github.com>
2023-11-15 15:53:57 +0300

    Add files via upload

---
+++ Portfolio/src/components/Footer.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import styled from 'styled-components';
+import FooterCol from './FooterCol';
+import PText from './PText';
+
+const FooterStyle = styled.div`
+  background-color: var(--deep-dark);
+  padding-top: 10rem;
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+  .copyright {
+    background-color: var(--dark-bg);
+    text-align: left;
+    padding: 1rem 0;
+    margin-top: 5rem;
+    .para {
+      margin-left: 0;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1 .para {
+      max-width: 100%;
+    }
+    .copyright {
+      .container {
+        div {
+          margin-top: 0;
+        }
+      }
+    }
+  }
+`;
+
+export default function Footer() {
+  return (
+    <FooterStyle>
+      <div className="container">
+        <div className="footer__col1">
+          <h1 className="footer__col1__title">Nazirjonov Nekruz</h1>
+          <PText>
+            I'm from Tajikistan. A place of beauty and nature. Ever since
+            school, I really loved programming and took part in various
+            Olympiads. I am confident that programming is my calling and am
+            willing to work hard to achieve my goals.
+          </PText>
+        </div>
+        <div className="footer__col2">
+          <FooterCol
+            heading="Important Links"
+            links={[
+              {
+                title: 'Home',
+                path: '/',
+                type: 'Link',
+              },
+              {
+                type: 'Link',
+                title: 'About',
+                path: '/about',
+              },
+              {
+                type: 'Link',
+                title: 'Projects',
+                path: '/projects',
+              },
+              {
+                type: 'Link',
+                title: 'Contact',
+                path: '/contact',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col3">
+          <FooterCol
+            heading="Contact Info"
+            links={[
+              {
+                title: '+7 911 287 82 03',
+                path: 'tel:+7 911 287 82 03',
+              },
+              {
+                title: 'nazirjonovnekruz@gmail.com',
+                path: 'mailto:nazirjonovnekruz@gmail.com',
+              },
+              {
+                title: 'GEC Circle, Saint Petersburg, Russia',
+                path: 'http://google.com/maps',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col4">
+          <FooterCol
+            heading="social Links"
+            links={[
+              {
+                title: 'Facebook',
+                path: 'http://facebook.com',
+              },
+              {
+                title: 'Twitter',
+                path: 'http://twitter.com',
+              },
+              {
+                title: 'Instagram',
+                path: 'http://instagram.com',
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <div className="container">
+          <PText>© 2023 - Nazirjonov Nekruz </PText>
+        </div>
+      </div>
+    </FooterStyle>
+  );
+}
---
+++ Portfolio/src/pages/About.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import styled from 'styled-components';
+import PText from '../components/PText';
+import Button from '../components/Button';
+import AboutImg from '../assets/images/about-page-img.png';
+import AboutInfoItem from '../components/AboutInfoItem';
+import ContactBanner from '../components/ContactBanner';
+
+const AboutPageStyles = styled.div`
+  padding: 20rem 0 10rem 0;
+
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-bottom: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export default function About() {
+  return (
+    <>
+      <AboutPageStyles>
+        <div className="container">
+          <div className="top-section">
+            <div className="left">
+              <p className="about__subheading">
+                Hi, I am <span>Nazirjonov Nekruz</span>
+              </p>
+              <h2 className="about__heading">Backend developer</h2>
+              <div className="about__info">
+                <PText>
+                  I am fom Tajikistan. I strive to do something useful for
+                  myself and cintribute in some way. I am a participant in
+                  various programming competitins.
+                  <br /> <br />
+                  Therefore, after graduating from school, I entered the
+                  university to major in information technology. I am confident
+                  that programming is my calling and I am willing to work hardto
+                  achieve my goals.
+                  <br />
+                  <br />
+                  My vision is to make the world a better place. Now almost
+                  everything is becoming better than ever. It is time for us to
+                  create more good stuff that helps the world to become a better
+                  place.
+                </PText>
+              </div>
+              <Button
+                btnText="Download CV"
+                btnLink="https://github.com/nekruz03/ITMO/blob/main/Resume/Nekruz_Nazirjonov_cv.pdf"
+              />
+            </div>
+            <div className="right">
+              <img src={AboutImg} alt="me" />
+            </div>
+          </div>
+          <div className="about__info__items">
+            <div className="about__info__item">
+              <h1 className="about__info__heading">Education</h1>
+
+              <AboutInfoItem title="School" items={['O.Juraev.Kanibadam']} />
+              <AboutInfoItem
+                title="Varsity"
+                items={[
+                  'Saint Petersburg State University of Information Technologies, Mechanics and Optics (ITMO)',
+                ]}
+              />
+            </div>
+            <div className="about__info__item">
+              <h1 className="about__info__heading">My Skills</h1>
+
+              <AboutInfoItem
+                title="FrontEnd"
+                items={['HTML', 'CSS', 'JavaScript', 'REACT']}
+              />
+              <AboutInfoItem
+                title="BackEnd"
+                items={['Node', 'Express', 'PHP']}
+              />
+              <AboutInfoItem
+                title="Design"
+                items={['Photoshop', 'After Effects', 'Adobe Premiere Pro']}
+              />
+            </div>
+            <div className="about__info__item" />
+          </div>
+        </div>
+        <ContactBanner />
+      </AboutPageStyles>
+    </>
+  );
+}
