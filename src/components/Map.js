90824b466714ee80241995bbcc3b9f87e058f331
Nekruz Nazirjonov <92411934+nekruz03@users.noreply.github.com>
2024-01-09 21:55:25 +0300

    creat branch dev

---
+++ src/components/Map.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import MapImg from '../assets/images/map.png';
+import PText from './PText';
+
+const MapStyles = styled.div`
+  background: url(${MapImg}) no-repeat;
+  background-position: center;
+  background-size: cover;
+  min-height: 400px;
+  .container {
+    position: relative;
+    min-height: 400px;
+  }
+  .map__card {
+    position: absolute;
+    right: 10%;
+    bottom: 10%;
+    padding: 2rem;
+    background: var(--deep-dark);
+    width: 100%;
+    max-width: 300px;
+    border-radius: 12px;
+  }
+  .map__card__heading {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  .map__card__link {
+    display: inline-block;
+    font-size: 1.6rem;
+    margin-top: 3rem;
+    text-decoration: underline;
+  }
+  @media only screen and (max-width: 768px) {
+    background-position: 80% center;
+  }
+  @media only screen and (max-width: 400px) {
+    .map__card {
+      max-width: none;
+      right: auto;
+    }
+  }
+`;
+
+export default function Map() {
+  return (
+    <MapStyles>
+      <div className="container">
+        <div className="map__card">
+          <h3 className="map__card__heading">Here is me</h3>
+          <PText>GEC circle, Saint - Petersburg, Russia</PText>
+          <a
+            className="map__card__link"
+            href="https://www.google.com/maps/place/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3/@59.9387695,29.4353772,9z/data=!3m1!4b1!4m6!3m5!1s0x4696378cc74a65ed:0x6dc7673fab848eff!8m2!3d59.9310584!4d30.3609096!16zL20vMDZwcjY?entry=ttu"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open in google map
+          </a>
+        </div>
+      </div>
+      {/* <img src={MapImg} alt="Map" /> */}
+    </MapStyles>
+  );
+}
